862d92667b59f318037af2a2f54c78aa486fbcc2
noahcao <herodan1023@gmail.com>
2018-07-16 09:09:05 +0800

    add docs for the first sprint report

--- code/dataTools/renameCSV.py
+++ code/dataTools/renameCSV.py
@@ -11,19 +11,21 @@ def countline(filename):
     return line
 
 def renamepathcsv(path):
+    sum = 0
     filedir = os.listdir(path)
     for filename in filedir:
-    begin = int(filename.split('_')[0])
-    file_path = path + filename
-    lines = countline(file_path)
-    end = begin + lines
-    newfilename = "%d_%d.csv" % (begin,end)
-    newfilepath = path + newfilename
-    os.rename(file_path, newfilepath)
-    print(file_path, begin, lines)
-
-def 
+        begin = int(filename.split('_')[0])
+        file_path = path + filename
+        lines = countline(file_path)
+        sum += lines
+        end = begin + lines
+        newfilename = "%d_%d.csv" % (begin,end)
+        newfilepath = path + newfilename
+        os.rename(file_path, newfilepath)
+        print(file_path, begin, lines)  
+    print("in total %d lines" % sum)
 
 if __name__=='__main__':
-    path = './data'
+    path = './data/'
+    renamepathcsv(path)
     
