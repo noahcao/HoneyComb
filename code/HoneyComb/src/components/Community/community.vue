36d2604321f79a8aedd415357372c71577b595d0
noahcao <herodan1023@gmail.com>
2018-07-17 17:01:07 +0800

    fix .gitignore misleading

---
+++ code/HoneyComb/src/components/Community/community.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav-bar></nav-bar>
+    <editor></editor>
+  </div>
+</template>
+
+<script>
+  import NavBar from '../main/NavBar'
+  import editor from './index'
+  export default {
+    name: 'outcomb',
+    components: {
+      NavBar,
+      editor
+    }
+  }
+</script>
+
+<style scoped>
+</style>
