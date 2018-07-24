30731d619c97eccfe003d16326123177ade8430e
noahcao <herodan1023@gmail.com>
2018-07-24 16:46:02 +0800

    add the button and comment modules for the poster panel

--- code/HoneyComb/src/components/Community/poster.vue
+++ code/HoneyComb/src/components/Community/poster.vue
@@ -1,18 +1,41 @@
 <template>
     <div id="posterpage">
         <nav-bar style='margin-bottom:1px'></nav-bar>
-        <div class="col-xs-8 col-sm-8" id="panel">
-            <div v-for="poster in posters" :key="poster.id">
-                <div class="jumbotron poster">
-                    <h2 class="h2-reponsive mb-3 blue-text">{{poster.title}}</h2>
-                    <a href="#">{{poster.username}}</a>posted at {{poster.posttime}}
-                    <hr class="my-4">
-                    <div v-html="poster.content" class='contentboard'>
+        <div class="col-xs-8 col-sm-8" id="panelboard">
+            <div class="jumbotron" id="panel">
+                <div class="row jumbotron" id="buttonbar">
+                    <div class="col-xs-8 col-sm-8">
+                        <h2 class="h2-reponsive mb-3 blue-text" id="paneltitle"><strong>Panel Topic</strong></h2>
                     </div>
-                    <div v-for="comment in poster.comments" :key="comment.id" class="commentboard">
-                        <div class="jumbotron jumbotron-fluid comment">
-                            <a href="#">{{comment.username}}</a> post at: {{comment.posttime}}
-                            <div v-html="comment.content">
+                    <div class="col-xs-4 col-sm-4" style="padding:0">
+                        <button type="button" class="btn btn-outline-warning waves-effect self-btn"><i class="fa fa-star pr-2" aria-hidden="true"> Star</i></button>
+                        <button type="button" class="btn btn-outline-info waves-effect px-3 self-btn"><i class="fa fa-pencil-square-o" aria-hidden="true"> Post</i></button>
+                        <button type="button" class="btn btn-outline-secondary waves-effect px-3 self-btn"><i class="fa fa-share-alt" aria-hidden="true"> Share</i></button>
+                    </div>
+                </div>
+                <div v-for="poster in posters" :key="poster.id">
+                    <div class="jumbotron poster">
+                        <h2 class="h2-reponsive mb-3 blue-text">{{poster.title}}</h2>
+                        <a href="#">{{poster.username}}</a>posted at {{poster.posttime}}
+                        <hr class="my-4">
+                        <div v-html="poster.content" class='contentboard'>
+                        </div>
+                        <div v-for="comment in poster.comments" :key="comment.id" class="commentboard">
+                            <div class="jumbotron jumbotron-fluid comment">
+                                <a href="#">{{comment.username}}</a> post at: {{comment.posttime}}
+                                <hr class="my-4" style="margin:0">
+                                <div v-html="comment.content" style="padding-left:10px;padding-right:10px">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row" id="newcomment">
+                            <div class="md-form input-group row" style="width:100%">
+                                <div class="col-xs-11 col-xm-11">
+                                    <input type="text" class="form-control" placeholder="Add a comment" aria-label="Add a comment" aria-describedby="basic-addon2">
+                                </div>
+                                <div class="col-xs-1 col-xm-1" style="float:right">
+                                <button class="btn btn-outline-primary waves-effect" type="button" style="padding:8px 15px 8px 15px;margin-left:10%;float:right"><i class="fa fa-pencil" aria-hidden="true"></i></button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -118,9 +141,24 @@ export default {
 #posterpage{
     height: 800px
 }
-#panel{
+#panelboard{
     height: 90%;
-    padding-top: 2%
+    padding-top: 2%;
+}
+#panel{
+    padding: 5px 5px 5px 5px;
+}
+#buttonbar{
+    padding-top:10px;
+    padding-bottom: 10px;
+    width: 100%;
+    margin-left:0%;
+    margin-bottom: 1%;
+}
+#paneltitle{
+    padding-top:0px;
+    font-size: 350%;
+    margin: 0
 }
 #rightwrapper{
     margin: 2% 0 0 0;
@@ -138,16 +176,22 @@ export default {
 .comment{
     width: 96%;
     margin: 10px 2% 0 2%;
-    padding: 1% 2% 1% 2%
+    padding: 0
+}
+.comment a{
+    padding: 10px !important;
+}
+.comment div{
+    padding-bottom: 10px
 }
 .jumbotron p{
     padding: 5px 10px 5px 10px
 }
-.jumbotron h2{
-    padding: 5px 10px 5px 10px
+.poster h2{
+    padding: 5px 10px 5px 20px
 }
-.jumbotron a{
-    padding: 5px 10px 0 10px
+.poster a{
+    padding: 5px 10px 0 25px
 }
 .poster{
     padding-top:  5px;
@@ -157,6 +201,15 @@ export default {
 .hr{
     padding: 0px;
     margin: 0px
+}
+.self-btn{
+    padding-left: 15px;
+    padding-right: 15px;
+    background-color: white !important;
+}
+#newcomment{
+    margin: 0px 3% 0px 3%;
+    width: 94%
 }
 </style>
 
