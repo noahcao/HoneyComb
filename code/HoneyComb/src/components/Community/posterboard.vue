61611b4d9a4b9582376bea6831733a7ac6da9afa
noahcao <herodan1023@gmail.com>
2018-07-24 08:38:03 +0800

    fix some bugs

--- code/HoneyComb/src/components/Community/posterboard.vue
+++ code/HoneyComb/src/components/Community/posterboard.vue
@@ -1,10 +1,10 @@
 <template>
     <div id="posterboard">
-        <div v-for="item in items" :key="item.id" class="posterwrapper">
-            <div class="col-xs-3 col-sm-3" style="height:100%">
+        <div v-for="item in items" :key="item.id">
+            <div class="col-xs-3 col-sm-3 posterwrapper">
                 <div class="poster">
                     <h3>Poster Title</h3>
-                    <a href="#">cjk</a>发布于<a>16分钟前</a>
+                    <a href="#">cjk</a><a>16分钟前</a>
                     <p></p>
                     <span class="badge badge-primary">Default</span>
                     <span class="badge badge-pill teal"><i class="fa fa-heart fa-2x" aria-hidden="true"></i></span>
@@ -12,7 +12,9 @@
                         Lorem ipsum dolor sit amet, consecteturfsdafsdsdadipisicing 
                         elit.dhsfjksdhfkjsdhfajkhdsjkfhdkjfsdafsdaf
                         fdsffsdfsdafsdfsdfsdfsdfsdafdsfdgsdagsdhdha
-                        fsdafsdafsdfdadsfdsfsdfasdfsdafdssadfsdafsdffdsafsdfsd</p>
+                        fsdafsdafsdfdadsfdsfsdfasdfsdafdssadfsdafsdffdsafsdfsddasd
+                        dasdsasadasdsdasdasddasdsaasdasdasdasdasdasdasdasdasdasdasdasda
+                        dasdasdasdasdasdasdasdasdasdasdasdasdasasdasd</p>
                 </div>
             </div>
         </div>
@@ -31,15 +33,20 @@ export default {
 
 <style scoped>
 @import '../../../static/css/mdb.css';
+*{
+    margin: 0px
+}
 .poster{
     background-color: white;
     border: solid;
     margin: 20px 5px 20px 5px;
-    padding: 2px 5px 2px 5px
+    padding: 2px 5px 2px 5px;
+    height: 80%
 }
 .posterwrapper{
     margin: 0;
-    height:45%
+    height: 45%;
+    width:25%
 }
 .postercontent{
     word-wrap:break-word;
