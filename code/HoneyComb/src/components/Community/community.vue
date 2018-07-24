06e06a484b32bdd19cb2e8e4406b5ba4aaa4b8cd
Clive2312 <Clive2312@users.noreply.github.com>
2018-07-24 09:18:21 +0800

    style fix

--- code/HoneyComb/src/components/Community/community.vue
+++ code/HoneyComb/src/components/Community/community.vue
@@ -2,7 +2,7 @@
 <template>
   <div id="community">
     <nav-bar style='margin-bottom:1px'></nav-bar>
-    <div class='row' style='height:100%'>
+    <div class="comBody">
       <div class="col-md-2" id='sidebar'>
         <sidebar></sidebar>
       </div>
@@ -14,38 +14,45 @@
 </template>
 
 <script>
-  /* eslint-disable */
-  import NavBar from '../main/NavBar'
-  import editor from './editor'
-  import sidebar from './sidebar'
-  import posterboard from './posterboard'
-  export default {
-    name: 'outcomb',
-    components: {
-      NavBar,
-      editor,
-      sidebar,
-      posterboard
-    }
+/* eslint-disable */
+import NavBar from '../main/NavBar'
+import editor from './editor'
+import sidebar from './sidebar'
+import posterboard from './posterboard'
+export default {
+  name: 'outcomb',
+  components: {
+    NavBar,
+    editor,
+    sidebar,
+    posterboard
   }
+}
 </script>
 
 <style scoped>
-  #community{
-    height: 750px;
-  }
-  #sidebar{
-    background-color: black;
-    height: 100%
-  }
-  #sidebar{
-    background-color: aqua;
-    height: 100%
-  }
-  #posterboard{
-    background-color: bisque;
-    height: 100%;
-    padding-left: 0;
-    padding-right: 0
-  }
+#community {
+  height: 750px;
+}
+#sidebar {
+  background-color: black;
+  height: 100%;
+}
+#sidebar {
+  background-color: aqua;
+  height: 100%;
+}
+#posterboard {
+  background-color: bisque;
+  height: 100%;
+  padding-left: 0;
+  padding-right: 0;
+}
+.comBody {
+  width: 100%;
+  position: absolute;
+  top: 70px;
+  bottom: 0px;
+  left: 0px;
+}
 </style>
--- code/HoneyComb/src/components/Community/posterboard.vue
+++ code/HoneyComb/src/components/Community/posterboard.vue
@@ -4,17 +4,16 @@
             <div class="col-xs-3 col-sm-3 posterwrapper">
                 <div class="poster">
                     <h3>Poster Title</h3>
-                    <a href="#">cjk</a><a>16分钟前</a>
+                    <a href="#">cjk</a>
+                    <a>16分钟前</a>
                     <p></p>
                     <span class="badge badge-primary">Default</span>
-                    <span class="badge badge-pill teal"><i class="fa fa-heart fa-2x" aria-hidden="true"></i></span>
+                    <span class="badge badge-pill teal">
+                        <i class="fa fa-heart fa-2x" aria-hidden="true"></i>
+                    </span>
                     <p class="postercontent">
-                        Lorem ipsum dolor sit amet, consecteturfsdafsdsdadipisicing 
-                        elit.dhsfjksdhfkjsdhfajkhdsjkfhdkjfsdafsdaf
-                        fdsffsdfsdafsdfsdfsdfsdfsdafdsfdgsdagsdhdha
-                        fsdafsdafsdfdadsfdsfsdfasdfsdafdssadfsdafsdffdsafsdfsddasd
-                        dasdsasadasdsdasdasddasdsaasdasdasdasdasdasdasdasdasdasdasdasda
-                        dasdasdasdasdasdasdasdasdasdasdasdasdasasdasd</p>
+                        Lorem ipsum dolor sit amet, consecteturfsdafsdsdadipisicing elit.dhsfjksdhfkjsdhfajkhdsjkfhdkjfsdafsdaf fdsffsdfsdafsdfsdfsdfssadflkasjldfjaklsdfhlkasjdfklajsdkfhalksjdfasddfsdafdsfdgsdagsdhdha fsdafsdafsdfdadsfdsfsdfasdfsdafdssadfsdafsdffdsafsdfsddasdasdfasdf dasdsasadasdsdasdasddasdsaasdasdasdasdasdasdasdasdasdasdasdasdaasdasdfasfasjkfajsdflkasjdkfhaksjfkhshdfkasjlkfjalksjdfklasjlkfjaskldjlahjfal dasdasdasdasdasdasdasdasdasdasdasdasdasasdasdsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+                    </p>
                 </div>
             </div>
         </div>
@@ -23,35 +22,39 @@
 
 <script>
 export default {
-    data(){
-        return{
-            items:[1,2,3,4,5,6,7,8]
+    data () {
+        return {
+            items: [1, 2, 3, 4, 5, 6, 7, 8]
         }
     }
 }
 </script>
 
 <style scoped>
-@import '../../../static/css/mdb.css';
-*{
-    margin: 0px
+@import "../../../static/css/mdb.css";
+* {
+  margin: 0px;
 }
-.poster{
-    background-color: white;
-    border: solid;
-    margin: 20px 5px 20px 5px;
-    padding: 2px 5px 2px 5px;
-    height: 80%
+.poster {
+  background-color: white;
+  border: solid;
+  margin: 20px 5px 20px 5px;
+  padding: 2px 5px 2px 5px;
+  height: 80%;
 }
-.posterwrapper{
-    margin: 0;
-    height: 45%;
-    width:25%
+.posterwrapper {
+  margin: 0;
+  height: 45%;
+  width: 25%;
 }
-.postercontent{
-    word-wrap:break-word;
-    text-overflow:ellipsis;
-    overflow:hidden;height:100%
+.postercontent {
+  word-wrap: break-word;
+  word-break: break-all;
+  display: -webkit-box;
+  -webkit-line-clamp: 11;
+  overflow: hidden;
+  -webkit-line-break: auto;
+  -webkit-box-orient: vertical;
 }
 </style>
 
