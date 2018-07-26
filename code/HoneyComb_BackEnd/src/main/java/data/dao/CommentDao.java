ff619fd2bff833eeb4f913123456d50aa6a80678
noahcao <herodan1023@gmail.com>
2018-07-26 10:32:14 +0800

    delete code files

--- code/HoneyComb_BackEnd/src/main/java/data/dao/CommentDao.java
+++
@@ -1,9 +0,0 @@
-package data.dao;
-
-import data.model.CommentEntity;
-
-public interface CommentDao {
-    void insert(String panelId, Integer postId, CommentEntity entity);
-
-    void deleteById(String panelId, Integer postId, Integer id);
-}
